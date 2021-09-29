d705bdb718af74dd0048075bad124cad2b4d5bcf
cliu268 <cliu268@gmail.com>
2021-09-28 23:54:11 -0400

    hw3 q2

--- hw3/q2.cpp
+++ hw3/q2.cpp
@@ -25,4 +25,55 @@ Sample Input：
 
 Sample Output：
 1
+
+Sample Input:
+5        
+1 1 1 0 0
+1 1 0 1 0
+1 0 1 0 0
+0 1 0 1 0
+0 0 0 0 1
+
+Sample Output:
+2
 */
+#include <iostream>
+#include <vector>
+using namespace std;
+void findFriends(vector<bool> &visted, vector<vector<int>> friends, int index) {
+    for (int i = 0; i < visted.size(); i++) {
+        if (i == index || visted[i]) continue;
+        if (friends[index][i] == 1) {
+            visted[i] = true;
+            findFriends(visted, friends, i);
+        }
+    }
+}
+
+int socialCircle(vector<vector<int>> friends) {
+    vector<bool> visted(friends.size(), false);
+    int numCircles = 0;
+    for (int i = 0; i < friends.size(); i++) {
+        if (!visted[i]) {
+            visted[i] = true;
+            numCircles += 1;
+            findFriends(visted, friends, i);
+        }
+    }
+    return numCircles;
+}
+
+int main(void){
+    int n;
+    cin >> n;
+    vector<vector<int>> friends(n);
+    for (int i = 0; i < n; i++) {
+        vector<int> row(n);
+        for (int j = 0; j < n; j++) {
+            cin >> row[j];
+        }
+        friends[i] = row;
+    }
+    cout << socialCircle(friends);
+    return 0;
+}
