400036639de2141debc00f54b311385311f69340
cliu268 <cliu268@gmail.com>
2021-10-08 01:01:31 -0400

    solving q2

--- hw4/q2.cpp
+++ hw4/q2.cpp
@@ -25,6 +25,103 @@ sample output 1：
 4
 4
 
+sample input 2：
+5 3
+01001
+10101
+00111
+11010
+01010
+1 1
+3 3
+5 5
+
+sample output 2：
+16
+7
+16
+
+sample input 3：
+3 2
+010
+101
+001
+1 1
+3 3
+
+sample output 3：
+7
+2
 Note：
 For 100% data, n <= 1000, m <= 100000
 */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printMaze(vector<vector<char>> maze) {
+    for (int i = 0; i < maze.size(); i++) {
+        for (int j = 0; j < maze.size(); j++) {
+            cout << maze[i][j];
+        }
+        cout << "\n";
+    }
+}
+
+bool isValid(vector<vector<char>> maze, int row, int col, char src) {
+    if (row < 0 || col < 0 || row >= maze.size() || col >= maze.size() || 
+        maze[row][col] == '*' || maze[row][col] == src) {
+        return false;
+    }
+    return true;
+}
+
+void solve(vector<vector<char>> &maze, int &count, int sr, int sc, char src) {
+    if (!isValid(maze, sr, sc, src)) {
+        return;
+    }
+
+    char src2 = maze[sr][sc];
+    maze[sr][sc] = '*';
+    count++;
+
+    solve(maze, count, sr+1, sc, src2);
+    solve(maze, count, sr-1, sc, src2);
+    solve(maze, count, sr, sc+1, src2);
+    solve(maze, count, sr, sc-1, src2);
+
+    return;
+}
+
+int main(void) {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<char>> maze(n);
+    for (int i = 0; i < n; i++) {
+        vector<char> row(n);
+        for (int x = 0; x < n; x++) {
+            cin >> row[x];
+        }
+        maze[i] = row;
+    }
+
+    vector<vector<int>> sp(m);
+    for (int j = 0; j < m; j++) {
+        vector<int> point(2);
+        cin >> point[0] >> point[1];
+        sp[j] = point;
+    }
+
+    for (int k = 0; k < m; k++) {
+        vector<vector<char>> maze2 = maze;
+        int count = 0;
+        char src = ' ';
+        solve(maze2, count, sp[k][0]-1, sp[k][1]-1, src);
+        cout << count << "\n";
+        // printMaze(maze);
+        // cout << "**********************" << "\n";
+        // printMaze(maze2);
+    }
+    return 0;    
+}
--- hw4/q3.cpp
+++ hw4/q3.cpp
@@ -19,3 +19,36 @@ Sample Output：
 3    -1   1    
 2    1    4 
 */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void traverse(vector<vector<int>> &board, int n, int m, int sr, int sc) {
+    // for (int i = 0; i < board.size(); i++) {
+    //     for (int j = 0; j < board[i].size(); j++) {
+    //         board[i][j] = sr*(-1);
+    //     }
+    // }    
+}
+
+void printBoard(vector<vector<int>> board) {
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[i].size(); j++) {
+            printf("%-*d", 5, board[i][j]);
+        }
+        cout << "\n";
+    }    
+}
+
+int main(void) {
+    int n, m, sr, sc;
+    cin >> n >> m >> sr >> sc;
+    vector<vector<int>> board(n);
+    for (int i = 0; i < n; i++) {
+        vector<int> row(m, 0);
+        board[i] = row;
+    }
+    traverse(board, n, m, sr, sc);
+    printBoard(board);
+    return 0;
+}
